b6156df18f9d92d4013124d097b8f72d2faae878
Mati Joseph <crucialniccur@gmail.com>
2025-05-17 16:13:55 +0300

    define the write_file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}', mode='w', encoding='utf-8') as a:
+        a.write({file_content})
+
 
 def append_file(file_name, append_content):
     pass
+
 
 def read_file(file_name):
     pass
