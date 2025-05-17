8f2f4934078b7d343d8c4c281fb47bf3bcdce587
Mati Joseph <crucialniccur@gmail.com>
2025-05-17 16:30:20 +0300

    remove some errors

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,14 +1,13 @@
 def write_file(file_name, file_content):
-    with open(f'{file_name}', mode='w', encoding='utf-8') as a:
-        a.write({file_content})
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as a:
+        a.write(file_content)
 
 
 def append_file(file_name, append_content):
-    with open(f"{file_name}", mode='a', encoding='utf-8') as b:
-        b.write({append_content})
+    with open(f"{file_name}.txt", mode='a', encoding='utf-8') as b:
+        b.write(append_content)
 
 
 def read_file(file_name):
-    with open(f"{file_name}", mode='r', encoding='utf-8') as c:
-        for line in c:
-            print(c)
+    with open(f"{file_name}.txt", mode='r', encoding='utf-8') as c:
+        return c.read()
